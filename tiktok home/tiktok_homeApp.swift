6f1f542195a202a47ddd413c5c80c5d125820df6
Rahul <rk1821389@gmail.com>
2023-04-03 22:29:43 +0530

    Initial Commit

---
+++ tiktok home/tiktok_homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktok_homeApp.swift
+//  tiktok home
+//
+//  Created by Rahul on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct tiktok_homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
